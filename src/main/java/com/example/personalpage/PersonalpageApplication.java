1b9add254c357045b997e6507c04e1f845ab7fa5
Adryellsz <adryellpaulo@gmail.com>
2023-10-24 15:50:12 -0300

    Restarted

---
+++ src/main/java/com/example/personalpage/PersonalpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personalpage/PersonalpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
