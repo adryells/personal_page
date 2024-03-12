d160fa00fc09bfda1032495c050e936c50eea60f
adryellsz <adryellpaulo@gmail.com>
2024-03-12 14:06:20 -0300

    ...

---
+++ src/main/kotlin/dev/adryell/Application.kt
@@ -0,0 +1,13 @@
+package dev.adryell
+
+import dev.adryell.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
