53092b90b58e8f26511580125703d8592b797328
adryellsz <adryellpaulo@gmail.com>
2024-03-16 12:37:25 -0300

    new refacto

--- src/main/kotlin/dev/adryell/models/Post.kt
+++
@@ -1,8 +0,0 @@
-package dev.adryell.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Post(val id: String, val title: String, val description: String)
-
-val PostStorage = mutableListOf<Post>()
