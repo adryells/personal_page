dc9e115f0e9c923a63c6ff9270a812eb36c40da1
Adryellsz <adryellpaulo@gmail.com>
2024-07-05 16:27:17 -0300

    vite

--- web/src/services/api.tsx
+++ web/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Use a variável de ambiente com o prefixo VITE_
   headers: {
     'Content-Type': 'application/json',
   },
