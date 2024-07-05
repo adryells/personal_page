4232dbb0247487ae9e2e7c4f267affd8362680e8
Adryellsz <adryellpaulo@gmail.com>
2024-07-05 14:29:59 -0300

    remove test

--- api/src/test/java/dev/adryell/personalpage/PersonalPageApplicationTests.java
+++ api/src/test/java/dev/adryell/personalpage/PersonalPageApplicationTests.java
@@ -1,13 +1,1 @@
 package dev.adryell.personalpage;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PersonalPageApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
