b072c747c6f0195ea668f6f1f9d37b9564fd742a
Paulo Adryell <40843657+adryells@users.noreply.github.com>
2024-07-06 13:21:17 -0300

    feat(api): Dockerizing application (#9)

--- api/src/main/java/dev/adryell/personalpage/PersonalPageApplication.java
+++ api/src/main/java/dev/adryell/personalpage/PersonalPageApplication.java
@@ -1,5 +1,6 @@
 package dev.adryell.personalpage;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -8,6 +9,12 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class PersonalPageApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(PersonalPageApplication.class, args);
 	}
 
