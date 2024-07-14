5bcff1e7c5807b452d8e0e9cdcea75b902f0a1ee
Paulo Adryell <40843657+adryells@users.noreply.github.com>
2024-07-14 13:58:13 -0300

    Refact(hybrid): Remove projects from db (#10)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,6 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import './i18n';
 import { AuthProvider } from './services/authContext';
-import LoginPage from './pages/Login';
-import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:id" element={<ProjectDetails />} /> 
-          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </AuthProvider>
